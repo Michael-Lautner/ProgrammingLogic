824566496199331f8617b80b00b4ea977ed4dd6b
“Michael-Lautner” <you@example.com>
2025-03-20 16:30:32 -0400

    Lab 1 progress

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,7 +1,20 @@
 using System;
 class Program
 {
- static void Main() {
-    Console.WriteLine("Hello, world!");
+ static void Main() 
+ //Step1: 
+ {
+    Console.WriteLine("Michael Lautner, IT-1050 - Lab 1");
+    Console.WriteLine("Second Line");
+    /* Michael Lautner 
+    IT-1050 - Lab 1*/
+    int favNum = 13;
+    string favLang = "Python";
+    double numProg = 2.0;
+    bool experience = true;
+    Console.WriteLine("My favorite number is " + favNum);
+    Console.WriteLine("My favorite programming language is " + favLang);
+    Console.WriteLine("I have written " + numProg + " programs before this class");
+    Console.WriteLine("Do I have programming experience? - > " + experience);
  }
 }
