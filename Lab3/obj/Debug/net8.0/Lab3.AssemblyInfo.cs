ff0f4d021e85c693da9f78f75f21e5a80cd36c3f
Michael-Lautner <156220713+Michael-Lautner@users.noreply.github.com>
2025-04-21 22:31:35 +0000

    Lab 5 Final 	new file:   Lab5/Lab5.csproj 	new file:   Lab5/Program.cs 	new file:   Lab5/bin/Debug/net8.0/Lab5 	new file:   Lab5/bin/Debug/net8.0/Lab5.deps.json 	new file:   Lab5/bin/Debug/net8.0/Lab5.dll 	new file:   Lab5/bin/Debug/net8.0/Lab5.pdb 	new file:   Lab5/bin/Debug/net8.0/Lab5.runtimeconfig.json 	new file:   Lab5/obj/Debug/net8.0/.NETCoreApp,Version=v8.0.AssemblyAttributes.cs 	new file:   Lab5/obj/Debug/net8.0/Lab5.AssemblyInfo.cs 	new file:   Lab5/obj/Debug/net8.0/Lab5.AssemblyInfoInputs.cache 	new file:   Lab5/obj/Debug/net8.0/Lab5.GeneratedMSBuildEditorConfig.editorconfig 	new file:   Lab5/obj/Debug/net8.0/Lab5.GlobalUsings.g.cs 	new file:   Lab5/obj/Debug/net8.0/Lab5.assets.cache 	new file:   Lab5/obj/Debug/net8.0/Lab5.csproj.CoreCompileInputs.cache 	new file:   Lab5/obj/Debug/net8.0/Lab5.csproj.FileListAbsolute.txt 	new file:   Lab5/obj/Debug/net8.0/Lab5.dll 	new file:   Lab5/obj/Debug/net8.0/Lab5.genruntimeconfig.cache 	new file:   Lab5/obj/Debug/net8.0/Lab5.pdb 	new file:   Lab5/obj/Debug/net8.0/Lab5.sourcelink.json 	new file:   Lab5/obj/Debug/net8.0/apphost 	new file:   Lab5/obj/Debug/net8.0/ref/Lab5.dll 	new file:   Lab5/obj/Debug/net8.0/refint/Lab5.dll 	new file:   Lab5/obj/Lab5.csproj.nuget.dgspec.json 	new file:   Lab5/obj/Lab5.csproj.nuget.g.props 	new file:   Lab5/obj/Lab5.csproj.nuget.g.targets 	new file:   Lab5/obj/project.assets.json 	new file:   Lab5/obj/project.nuget.cache 	modified:   ProgrammingLogic.sln

--- Lab3/obj/Debug/net8.0/Lab3.AssemblyInfo.cs
+++ Lab3/obj/Debug/net8.0/Lab3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb64bbab71e053ba27d8a5be2af62c080780b8e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c89b2eed436596de9f5e7e897f01479549156d3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Lab5/Program.cs
@@ -0,0 +1,78 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Lab5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Problem 1: Seasons of the Year
+
+        string[] seasons = new string[4] {"Spring", "Summer", "Fall", "Winter"};
+
+        foreach (string season in seasons)
+        {
+            Console.WriteLine(season);
+        }
+
+        //Problem 2: Days of the Week
+
+        string[] days = new string[7] {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+
+        int day = 0;
+        
+        do
+            {
+            Console.WriteLine("Enter a number 1 - 7");
+            day = Convert.ToInt32(Console.ReadLine());
+        
+            }
+        while (day >= 8);
+
+        Console.WriteLine("That day is: " + days[day-1]);
+
+
+        //Problem 3: Favorite Book and Authors
+
+        string[] books = new string[3] {"Angela's Ashes", "The Indifferent Stars Above", "The Faithful Executioner"};
+        string[] authors = new string[3] {"Frank McCourt", "Daniel Brown", "Joel Harrington"};
+
+        for (int i = 0; i <3; i++)
+            {
+                Console.WriteLine(books[i] + " by " + authors[i]);
+            }
+
+            //Problem 4
+
+            int[] temperatures = new int[5] {65, 72, 78, 70, 68};
+
+            Array.Sort(temperatures);
+
+            for (int i = 0; i<5; i++)
+            {  
+                Console.WriteLine(temperatures[i]);
+            }
+            
+        
+
+            Console.WriteLine(temperatures[0]);
+            Console.WriteLine(temperatures[temperatures.Length-1]);
+            
+
+        //Problem 5: Reverse Countdown
+
+        int[] countdown = new int[5] { 5,4,3,2,1};
+
+        Array.Reverse(countdown);
+
+        for (int i = 0; i<5; i++)
+        {
+            Console.WriteLine(countdown[i]);
+        }
+
+
+
+
+
+    }
+}
