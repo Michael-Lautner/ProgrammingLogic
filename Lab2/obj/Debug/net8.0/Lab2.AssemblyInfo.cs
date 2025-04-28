2d3df009f839cf3ce4c7b6937f3aa60dbd5337a9
Michael-Lautner <156220713+Michael-Lautner@users.noreply.github.com>
2025-04-28 23:37:54 +0000

    Lab 6 Final

--- Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
+++ Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c89b2eed436596de9f5e7e897f01479549156d3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff0f4d021e85c693da9f78f75f21e5a80cd36c3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
